e46636deda239d823a283d594a29516989f64efe
João Seixas <ed-01@hotmail.com>
2014-10-16 21:47:34 +0100

    Bug that cause the program to fail sometimes fixe. Code commented and refactored

--- src/lcs-serial.c
+++ src/lcs-serial.c
@@ -4,11 +4,15 @@
 
 #define MAX(x, y) ( ((x) > (y)) ? (x) : (y) )
 
+//Board is a matrix MxN, where M is height and N is width.
+//The horizotal string will be in horizontalString
+//Vertical string in verticalString
+
 typedef struct {
 	int height;
 	int width;
-	char* vectorHeight;
-	char* vectorWeidth;
+	char* horizontalString;
+	char* verticalString;
 	int** matrix;
 }board_t;
 
@@ -17,9 +21,9 @@ typedef board_t* Board;
 short cost(int x);
 Board parseFile(char* fileName);
 void processCell(int i, int j, Board board);
-void printResults(board_t* board);
-void iterateBoard(board_t* board);
-void cleanAll(board_t* board);
+void printResults(Board board);
+void iterateBoard(Board board);
+void cleanAll(Board board);
 
 int main(int argc, char* argv[]){
 
@@ -31,32 +35,41 @@ int main(int argc, char* argv[]){
 	return 0;
 }
 
+
+//This function applies the algorithm to the given cell on the board
+//Arguments: i, the column of the cell. j, the row of the cell. board, the current board.
 void processCell(int i, int j, Board board){
-	if (i == 0 || j == 0) {
+	if (i == 0 || j == 0) {                                                             //First it checks if either i=0 or j=0, if so, we are in the first line/column so it must be filled with 0's
 		board->matrix[i][j] = 0;
-	} else if (board->vectorHeight[i] == board->vectorWeidth[j]) {
-		board->matrix[i][j] = board->matrix[i - 1][j - 1] + cost(i+j);
-	}else{
+	} else if (board->horizontalString[i] == board->verticalString[j]) {                //Else, if there is a match, the current cell will be given the value of the diagonally left cell + the cost function (that is always 1)
+		board->matrix[i][j] = board->matrix[i - 1][j - 1] + 1;//cost(i+j);
+	}else{                                                                              //Else, means no match, the cell will be given the value of the highest of either the left cell or the top cell
 		board->matrix[i][j] = MAX(board->matrix[i - 1][j], board->matrix[i][j - 1]);
 	}
 }
-void iterateBoard(board_t* board){
-	int heightLength = board->height + 1;
-	int widthLength = board->width + 1;
+
+//This function iterates through the cells of the given board
+//Arguments: board, the current board.
+void iterateBoard(Board board){
+	int horizontalStringLenght = board->height + 1;
+	int verticalStringLenght = board->width + 1;
 	size_t i, j;
-	for (i = 0; i < heightLength; ++i) {
-		for (j = 0; j < widthLength; ++j) {
-			processCell(i, j, board);
+	for (i = 0; i < horizontalStringLenght; ++i) {
+		for (j = 0; j < verticalStringLenght; ++j) {
+			processCell(i, j, board);                       //for each cell, process it.
 		}
 	}
 }
+
+//Function intended to build the board, given a file
+//Arguments: fileName, the name of the file to be read
 Board parseFile(char* fileName){
 	FILE* file;
 	int height;
 	int width;
 	int c;
-	char* vectorHeight;
-	char* vectorWeidth;
+	char* horizontalString;
+	char* verticalString;
 	int** matrix;
 	size_t n = 1;
 
@@ -69,19 +82,19 @@ Board parseFile(char* fileName){
 	    printf("Could not read height and width");
 	    exit(3);
 	}
-	if(!(vectorHeight = (char*) malloc(sizeof(char)*height + 1))||
-	   !(vectorWeidth = (char*) malloc(sizeof(char)*width + 1))){
+	if(!(horizontalString = (char*) malloc(sizeof(char)*height + 1))||
+	   !(verticalString = (char*) malloc(sizeof(char)*width + 1))){
 		    printf("Error allocating row pointers for board.\n");
 		    exit(4);
 	}
 
 	while((c = fgetc(file)) != '\n'){
-		 vectorHeight[n++] = (char)c;
+		 horizontalString[n++] = (char)c;
 	}
 
 	n = 1;
 	while((c = fgetc(file)) != '\n'){
-		vectorWeidth[n++] = (char)c;
+		verticalString[n++] = (char)c;
 	}
 
 
@@ -97,35 +110,53 @@ Board parseFile(char* fileName){
 	result->height =  height;
 	result->width = width;
 	result->matrix = matrix;
-	result->vectorHeight = vectorHeight;
-	result->vectorWeidth = vectorWeidth;
+	result->horizontalString = horizontalString;
+	result->verticalString = verticalString;
 
 	return result;
 
 }
-void printResults(board_t* board){
-	int heightLength = board->height;
-	int widthLength = board->width;
+
+//This function performs the backtrack on the matrix, while filling the Longest Common Subsequence. Also prints the supposed output
+//Arguments: board, the current board
+void printResults(Board board){
+	int horizontalStringLenght = board->height;
+	int verticalStringLenght = board->width;
 	size_t i, j;
-	int aux, finalSize = board->matrix[heightLength][widthLength];
+	int aux, finalSize = board->matrix[horizontalStringLenght][verticalStringLenght];           //finalsize, the last cell of the board, containing the size of the substring
 	char* subsequence = (char*) malloc(sizeof(char) * finalSize);
 	int** matrix = board->matrix;
+	char* horizontalString = board->horizontalString;
+	char* vectorWidth =board->verticalString;
 
+	/* just for testing */
+/*	for (i = 0; i < horizontalStringLenght; ++i) {
+		for (j = 0; j < verticalStringLenght; ++j) {
+			printf("|%d|", board->matrix[i][j]);
+		}
+		printf("\n");
+	}
+*/
 	aux = finalSize;
 	while(aux > 0){
-		if((matrix[heightLength-1][widthLength] != aux) &&
-		   (matrix[heightLength][widthLength-1] != aux)){
-			subsequence[aux -1] = board->vectorHeight[heightLength];
-			heightLength--;
-			widthLength--;
+		if((matrix[horizontalStringLenght-1][verticalStringLenght] != aux) &&
+		   (matrix[horizontalStringLenght][verticalStringLenght-1] != aux)){            //if the left value, or the top value are lower: match
+			subsequence[aux -1] = board->horizontalString[horizontalStringLenght];      //add string to the subsequence
+			horizontalStringLenght--;
+			verticalStringLenght--;
 			aux--;
-		}else if (matrix[heightLength][widthLength-1] == aux) {
-			widthLength--;
-		}else if (matrix[heightLength-1][widthLength] == aux) {
-			heightLength--;
+		}else if (horizontalString[horizontalStringLenght] == vectorWidth[verticalStringLenght]) {  //if there is a match, move diagonally
+			subsequence[aux -1] = board->horizontalString[horizontalStringLenght];                   //add the value to the subsequence
+			horizontalStringLenght--;
+			verticalStringLenght--;
+			aux--;
+		}else if (matrix[horizontalStringLenght][verticalStringLenght-1] == aux) {
+			verticalStringLenght--;
+		}else if (matrix[horizontalStringLenght-1][verticalStringLenght] == aux) {
+			horizontalStringLenght--;
 		}
 	}
-	printf("%d\n", finalSize);
+	printf("%d\n", finalSize);                              //print the final output
 	for (aux = 0; aux < finalSize; ++aux) {
 		printf("%c", subsequence[aux]);
 	}
@@ -133,15 +164,11 @@ void printResults(board_t* board){
 
 	free(subsequence);
 	matrix = NULL;
-/*	for (i = 0; i < heightLength; ++i) {
-		for (j = 0; j < widthLength; ++j) {
-			printf("|%d|", board->matrix[i][j]);
-		}
-		printf("\n");
-	}
- just for testing */
+
 
 }
+
+//Function to add computation so speedups can be seen in parallel mode. Always returns 1
 short cost(int x){
 	int i, n_iter = 20;
 	double dcost = 0;
@@ -149,11 +176,13 @@ short cost(int x){
 		dcost += pow(sin((double) x),2) + pow(cos((double) x),2);
 	return (short) (dcost / n_iter + 0.1);
 }
-void cleanAll(board_t* board){
+
+//Function to clear the resources
+void cleanAll(Board board){
 	size_t n;
 
-	free(board->vectorHeight);
-	free(board->vectorWeidth);
+	free(board->horizontalString);
+	free(board->verticalString);
 
 	for(n = 0; n < board->height; n++){
 		free(board->matrix[n]);
